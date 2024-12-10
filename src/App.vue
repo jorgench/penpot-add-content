295850d4b7d97964994312eb62401067b782cd2f
Jorge Naventa <jmnch0@gmail.com>
2024-12-10 12:10:55 -0500

    chore: init repository

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const theme = ref<string | null>(null)
+
+onMounted(() => {
+  const url = new URL(window.location.href)
+
+  const initialTheme = url.searchParams.get('theme')
+
+  if (initialTheme) {
+    theme.value = initialTheme as string
+  }
+})
+</script>
+
+<template>
+  <main :data-theme="theme"></main>
+</template>
+
+<style scoped></style>
